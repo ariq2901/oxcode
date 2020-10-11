ae29d18c7aa9cf167f0111ba73a7915da276c113
ariq2901 <ariq2901@gmail.com>
2020-10-11 21:47:48 +0700

    responsive layout

--- src/components/ComponentHome/Nav.jsx
+++ src/components/ComponentHome/Nav.jsx
@@ -4,7 +4,18 @@ import '../../App.css';
 
 
 const Nav = () => {
+  const [navbar, setNavbar] = React.useState(false);
   const [hamburger, setHamburger] = React.useState(false);
+
+  const changeNavbar = () => {
+    if(window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  }
+
+  window.addEventListener('scroll', changeNavbar);
 
   const onHamburger = () => {
     setHamburger(!hamburger)
@@ -13,30 +24,30 @@ const Nav = () => {
 
   return(
     <Fragment>
-      <nav>
+      <nav className={navbar ? 'nav-scrolled' : ''}>
         <div className="container">
           <div className="row row-nav">
-          <ul className={hamburger ? "nav-links slide" : "nav-links"}>
-            <li><NavLink activeClassName="navbar__link--active" className="navbar__link" exact={true} to="/">home</NavLink></li>
-            <li><NavLink activeClassName="navbar__link--active" className="navbar__link" to="list-attraction">list attractions</NavLink></li>
-            <li className="dropdown">
-              <label htmlFor="megamenu-check" className="megamenu-check-label">
-                category
-              </label>
-              <div className="menu">
-                <input type="checkbox" id="megamenu-check"/>
-                <i className="fas fa-chevron-down arrow-bawah"></i>
-              </div>
-            </li>
-            <li><NavLink activeClassName="navbar__link--active" className="navbar__link" to="/about">about us</NavLink></li>
-            <li className="btn-log"><NavLink className="btn-nav-login" to="/login">login</NavLink></li>
-          </ul>
-          <div className="menu-toggle">
-            <input type="checkbox" onClick={onHamburger}/>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+            <ul className={hamburger ? "nav-links slide" : "nav-links"}>
+              <li><NavLink activeClassName="navbar__link--active" className="navbar__link" exact={true} to="/">home</NavLink></li>
+              <li><NavLink activeClassName="navbar__link--active" className="navbar__link" to="list-attraction">list attractions</NavLink></li>
+              <li className="dropdown">
+                <label htmlFor="megamenu-check" className="megamenu-check-label">
+                  category
+                </label>
+                <div className="menu">
+                  <input type="checkbox" id="megamenu-check"/>
+                  <i className="fas fa-chevron-down arrow-bawah"></i>
+                </div>
+              </li>
+              <li><NavLink activeClassName="navbar__link--active" className="navbar__link" to="/about">about us</NavLink></li>
+              <li className="btn-log"><NavLink className="btn-nav-login" to="/login">login</NavLink></li>
+            </ul>
+            <div className="menu-toggle">
+              <input type="checkbox" onClick={onHamburger}/>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
         </div>
       </nav>
--- src/components/ComponentHome/PopularSpots.jsx
+++ src/components/ComponentHome/PopularSpots.jsx
@@ -5,86 +5,85 @@ import Waterbom from '../../img/waterbom.jpg';
 import Dufan from '../../img/dufan.jpg';
 import { NavLink } from 'react-router-dom';
 
-class PopularSpots extends Component {
-  render() {
-    return(
-      <Fragment>
-        <div className="spots-wrapper">
-          <div className="spots-title">
-            <span>most popular tourist spots</span>
-            <p href="#">view all</p>
-          </div>
-          <div className="spots-list">
-            <NavLink className="crd" to="/detail">
-              <div className="img-wrapper">
-                <img src={Bromo} alt="bromo img"/>
-              </div>
-              <div className="title-wrapper">
-                <span>bromo mountain</span>
-              </div>
-              <div className="rate-wrapper">
-                <div className="rating">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                </div>
-                <p className="total-reviews">175 reviews</p>
-              </div>
-              <div className="location-wrapper">
-                <i class="fas fa-map-marker-alt"></i>
-                <p className="location-name">bromo</p>
-              </div>
-            </NavLink>
-            <NavLink className="crd" to="/detail">
-              <div className="img-wrapper">
-                <img src={Waterbom} alt="waterbom img"/>
-              </div>
-              <div className="title-wrapper">
-                <span>waterbom jakarta</span>
-              </div>
-              <div className="rate-wrapper">
-                <div className="rating">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-                </div>
-                <p className="total-reviews">340 reviews</p>
-              </div>
-              <div className="location-wrapper">
-                <i class="fas fa-map-marker-alt"></i>
-                <p className="location-name">jakarta</p>
-              </div>
-            </NavLink>
-            <NavLink className="crd" to="/detail">
-              <div className="img-wrapper">
-                <img src={Dufan} alt="dufan img"/>
-              </div>
-              <div className="title-wrapper">
-                <span>dunia fantasi</span>
-              </div>
-              <div className="rate-wrapper">
-                <div className="rating">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                </div>
-                <p className="total-reviews">439 reviews</p>
-              </div>
-              <div className="location-wrapper">
-                <i class="fas fa-map-marker-alt"></i>
-                <p className="location-name">jakarta</p>
-              </div>
-            </NavLink>
-          </div>
+const PopularSpots = () => {
+
+  return(
+    <Fragment>
+      <div className="spots-wrapper">
+        <div className="spots-title">
+          <span>most popular tourist spots</span>
+          <p href="#">view all</p>
         </div>
-      </Fragment>
-    );
-  }
+        <div className="spots-list">
+          <NavLink className="crd" to="/detail">
+            <div className="img-wrapper">
+              <img src={Bromo} alt="bromo img"/>
+            </div>
+            <div className="title-wrapper">
+              <span>bromo mountain</span>
+            </div>
+            <div className="rate-wrapper">
+              <div className="rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <p className="total-reviews">175 reviews</p>
+            </div>
+            <div className="location-wrapper">
+              <i class="fas fa-map-marker-alt"></i>
+              <p className="location-name">bromo</p>
+            </div>
+          </NavLink>
+          <NavLink className="crd" to="/detail">
+            <div className="img-wrapper">
+              <img src={Waterbom} alt="waterbom img"/>
+            </div>
+            <div className="title-wrapper">
+              <span>waterbom jakarta</span>
+            </div>
+            <div className="rate-wrapper">
+              <div className="rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+              </div>
+              <p className="total-reviews">340 reviews</p>
+            </div>
+            <div className="location-wrapper">
+              <i class="fas fa-map-marker-alt"></i>
+              <p className="location-name">jakarta</p>
+            </div>
+          </NavLink>
+          <NavLink className="crd" to="/detail">
+            <div className="img-wrapper">
+              <img src={Dufan} alt="dufan img"/>
+            </div>
+            <div className="title-wrapper">
+              <span>dunia fantasi</span>
+            </div>
+            <div className="rate-wrapper">
+              <div className="rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <p className="total-reviews">439 reviews</p>
+            </div>
+            <div className="location-wrapper">
+              <i class="fas fa-map-marker-alt"></i>
+              <p className="location-name">jakarta</p>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 export default PopularSpots;
