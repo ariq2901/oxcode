9a73dc4bde06e2db492b328958bb2c2791343d54
ariq2901 <ariqkage@gmail.com>
2020-10-18 11:41:07 +0700

    Megamenu finish

--- src/components/ComponentAbout/AboutMain.jsx
+++ src/components/ComponentAbout/AboutMain.jsx
@@ -1,19 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 // import {DisplayMapFC} from '../../DisplayMapFC';
 import Dummy from '../../img/man-dummy.png';
 import Email from '../../svg/email.svg';
 import Facebook from '../../svg/facebook.svg';
 import Instagram from '../../svg/instagram.svg';
 import Youtube from '../../svg/youtube.svg';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 const AboutMain = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+
+
   return(
     <Fragment>
       <div className="started-wrapper">
-        <div className="started-title">
+        <div className="started-title" data-aos="fade-right">
           <p>how it all started?</p>
         </div>
-        <div className="started-text">
+        <div className="started-text" data-aos="fade-up" data-aos-delay="300">
           <p>
             blabla is a trusted travel agent who has been in the field for almost 12 years,
             this company was founded by daffa rabbanee who has experience in the field of 
@@ -22,10 +29,10 @@ const AboutMain = () => {
           </p>
         </div>
       </div>
-      <div className="our-team-title">
+      <div className="our-team-title" data-aos="fade-right" data-aos-offset="350">
         <p>our team</p>
       </div>
-      <div className="our-team-grid">
+      <div className="our-team-grid" data-aos="fade-up" data-aos-offset="310">
         <div className="person-card">
           <div className="person-img-wrapper">
             <img src={Dummy} alt="person card"/>
@@ -67,37 +74,45 @@ const AboutMain = () => {
         </div>
       </div>
       <div className="contact">
-        <div className="map-wrapperaj">
+        <div className="map-wrapperaj" data-aos="zoom-out" data-aos-offset="400">
           {/* <DisplayMapFC lat="-6.175522" longi="106.827175" /> */}
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.2241183703695!2d107.00565171434327!3d-6.493282565289409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69bdb8f6c50289%3A0xb7a33d4d5721ad5f!2sSMK%20MADINATULQURAN!5e0!3m2!1sen!2sid!4v1602928798982!5m2!1sen!2sid" frameborder="0" className="map-contact" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          <iframe title="our location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.2241183703695!2d107.00565171434327!3d-6.493282565289409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69bdb8f6c50289%3A0xb7a33d4d5721ad5f!2sSMK%20MADINATULQURAN!5e0!3m2!1sen!2sid!4v1602928798982!5m2!1sen!2sid" frameborder="0" className="map-contact" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div>
         <div className="contact-info">
-          <div className="info-title">
+          <div className="info-title" data-aos="fade-up" data-aos-delay="500" data-aos-offset="400">
             <p>contact us</p>
           </div>
-          <div className="social-media">
-            <div className="info-social">
-              <img src={Email} className="icon-svg" alt="email"/>
-              <p>info@smkmadinatulquran.sch.id</p>
-            </div>
+          <div className="social-media" data-aos="fade-up" data-aos-delay="800" data-aos-offset="500">
+            <a href="mailto:info@smkmadinatulquran.sch.id">
+              <div className="info-social">
+                <img src={Email} className="icon-svg" alt="email"/>
+                <p>info@smkmadinatulquran.sch.id</p>
+              </div>
+            </a>
           </div>
-          <div className="social-media">
-            <div className="info-social">
-              <img src={Facebook} className="icon-svg" alt="facebook"/>
-              <p>smktimadinatulquran</p>
-            </div>
+          <div className="social-media" data-aos="fade-up" data-aos-delay="1100" data-aos-offset="500">
+            <a href="https://www.facebook.com/smktimadinatulquran" target="_blank">
+              <div className="info-social">
+                <img src={Facebook} className="icon-svg" alt="facebook"/>
+                <p>smktimadinatulquran</p>
+              </div>
+            </a>
           </div>
-          <div className="social-media">
-            <div className="info-social">
-              <img src={Youtube} className="icon-svg" alt="youtube"/>
-              <p>smk madinatulquran</p>
-            </div>
+          <div className="social-media" data-aos="fade-up" data-aos-delay="1400" data-aos-offset="500">
+            <a href="https://www.youtube.com/channel/UCLQ2_4V-t11pUG0pATDXK6g" target="_blank">
+              <div className="info-social">
+                <img src={Youtube} className="icon-svg" alt="youtube"/>
+                <p>smk madinatulquran</p>
+              </div>
+            </a>
           </div>
-          <div className="social-media">
-            <div className="info-social">
-              <img src={Instagram} className="icon-svg" alt="instagram"/>
-              <p>smktimadinatulquran</p>
-            </div>
+          <div className="social-media" data-aos="fade-up" data-aos-delay="1700" data-aos-offset="500">
+            <a href="https://www.instagram.com/smktimadinatulquran/" target="_blank">
+              <div className="info-social">
+                <img src={Instagram} className="icon-svg" alt="instagram"/>
+                <p>smktimadinatulquran</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
--- src/components/ComponentHome/Nav.jsx
+++ src/components/ComponentHome/Nav.jsx
@@ -1,12 +1,52 @@
 import React, { Component, Fragment } from 'react';
 import {NavLink} from 'react-router-dom';
 import '../../App.css';
-
+import Groupone from '../../img/icon/groupone.png';
+import Axios from 'axios';
+import { config } from '../../config';
 
 const Nav = () => {
+  const [category, setCategory] = React.useState([]);
+  const [megamenu, SetMegamenu] = React.useState(false);
   const [navbar, setNavbar] = React.useState(false);
   const [hamburger, setHamburger] = React.useState(false);
 
+  const getCategory = async () => {
+    try {
+      const respon = await Axios.get(`${config.api_host}/api/categories`);
+      // setList(respon.data);
+      setCategory(respon.data.data);
+      
+    } catch(e) {
+      console.error('error feching data', e);
+    }
+  }
+
+  // const getImageById = () => {
+  //   try {
+  // }
+  // catch(e) {
+  //   console.error('error fetching', e);
+  // }
+  //   // console.log('calist', calist);
+  //   // console.log('categories ', category);
+  // }
+
+  // const getImage = () => {
+  //   const imageMap = category.map((item) => item.image);
+  //   const image = imageMap.map((icon) => icon.id);
+  //   const imageData = category.map((data) => data.push(image));
+  //   // console.log('image', image);
+  //   // imageData.push(image);
+  //   console.log('category final', imageData);
+  // }
+  
+  React.useEffect(() => {
+    getCategory();
+  }, []);
+
+
+  
   const changeNavbar = () => {
     if(window.scrollY >= 80) {
       setNavbar(true);
@@ -14,17 +54,21 @@ const Nav = () => {
       setNavbar(false);
     }
   }
-
+  
   window.addEventListener('scroll', changeNavbar);
 
   const onHamburger = () => {
-    setHamburger(!hamburger)
-    console.log(hamburger);
+    setHamburger(!hamburger);
   }
+
+  const onMegamenu = () => {
+    SetMegamenu(!megamenu);
+  }
+
 
   return(
     <Fragment>
-      <nav className={navbar ? 'nav-scrolled' : ''}>
+      <nav className={ navbar ? 'nav-scrolled' : megamenu ? 'nav-white' : ''}>
         <div className="container">
           <div className="row row-nav">
             <ul className={hamburger ? "nav-links slide" : "nav-links"}>
@@ -35,7 +79,7 @@ const Nav = () => {
                   category
                 </label>
                 <div className="menu">
-                  <input type="checkbox" id="megamenu-check"/>
+                  <input type="checkbox" onClick={onMegamenu} id="megamenu-check"/>
                   <i className="fas fa-chevron-down arrow-bawah"></i>
                 </div>
               </li>
@@ -47,6 +91,48 @@ const Nav = () => {
               <span></span>
               <span></span>
               <span></span>
+            </div>
+          </div>
+        </div>
+        <div className={megamenu ? "megamenu" : "megamenu-hidden"}>
+          <div className="container pt-4">
+            <div className="megamenu-title">
+              <div className="p-categories">
+                <p className="popular-title">popular categories</p>
+                <p className="popular-link">view all</p>
+              </div>
+              <div className="p-searches">
+                <p className="popular-searches">popular searches</p>
+              </div>
+            </div>
+            <div className="megamenu-main">
+              {console.log(category)}
+              <div className="category-list">
+              {category.map((item) => 
+                <div className="category-wrapper">
+                  <img src={`${config.api_host}/api/images/${item.image.id}`} alt="icon" />
+                  <p>{item.name}</p>
+                </div>
+              )}
+              </div>
+              <div className="search-list">
+                <div className="search-wrapper">
+                  <i class="fas fa-search"></i>
+                  <p>bromo</p>
+                </div>
+                <div className="search-wrapper">
+                  <i class="fas fa-search"></i>
+                  <p>dufan</p>
+                </div>
+                <div className="search-wrapper">
+                  <i class="fas fa-search"></i>
+                  <p>ancol</p>
+                </div>
+                <div className="search-wrapper">
+                  <i class="fas fa-search"></i>
+                  <p>taman safari</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
